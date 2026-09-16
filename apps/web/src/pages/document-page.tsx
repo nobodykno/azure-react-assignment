461e7590c031f5b25bf22e442f18a8eaec79f031
paramjitjena <paramjit@goodseeker.com>
2026-09-16 19:58:35 +0530

    fix(ui): update headline text

--- apps/web/src/pages/document-page.tsx
+++ apps/web/src/pages/document-page.tsx
@@ -119,7 +119,7 @@ export default function ProcessedDocuments() {
         </h1>
 
         <p className="documents-description">
-          View clinical files and its measurements.
+          View clinical documents and its measurements.
         </p>
       </div>
 
